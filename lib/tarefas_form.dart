4eed19a8b83f928d4dec2db7f7280823dc04ed25
Vitor Campione <vitor.campione@etec.sp.gov.br>
2023-08-21 14:49:00 -0300

    arquivos modificados

--- lib/tarefas_form.dart
+++ lib/tarefas_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import 'tarefa_model.dart';
+
 class TarefasForm extends StatefulWidget {
   const TarefasForm({super.key});
 
@@ -9,57 +11,73 @@ class TarefasForm extends StatefulWidget {
 }
 
 class _TarefasFormState extends State<TarefasForm> {
-  final _dateController = TextEditingController();  // controla o estado do campo da data 
+  final _dateController = TextEditingController(); // controla o estado do campo da data
 
-  final _formKey = GlobalKey<FormState>();  // Controla o estado do formulário
-
+  final _formKey = GlobalKey<FormState>(); // 1- Controla o estado do formulário
+  final _tarefa = Tarefa(descricao: "", prazo: DateTime.now()); // 6-Objeto que irá receber os dados da nova tarefa
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-        child: Form(
-
-            key: _formKey,  // relaciona o controlador de estado do formulário
-
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    label: Text("Descrição"), border: OutlineInputBorder()),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: _dateController, // controlador associado
-                readOnly: true,
-                onTap: () async {
-                  var data = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime.now().add(Duration(days: 360)));
-                  print(data);
-                  if (data != null) {
-                    String dataFormata =
-                        DateFormat("dd-MM-yyyy").format(data);
-                    _dateController.text = dataFormata;
-                  }
-                },
-                keyboardType: TextInputType.datetime,
-                decoration: InputDecoration(
-                    label: Text("Prazo"), border: OutlineInputBorder()),
-              ),
-              const SizedBox(height: 10,),
-              ElevatedButton(onPressed: () {
-
-                // executa a validação do formulário
-                if (_formKey.currentState!.validate()){
-                    _formKey.currentState!.save();  // solicita ao formulário que salve os dados 
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      child: Form(
+        
+        key: _formKey, //2- relaciona o controlador de estado do formulário
+        
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                  label: Text("Descrição"), border: OutlineInputBorder()),
+              validator: (value) => (value??"").isEmpty?"Uma descrição para a tarefa deve ser informada.":null,  // 4- Validador do campo descrição
+              onSaved: (value)=>_tarefa.descricao = value!, // 7- Recebendo o valor do campo e armazenando na propriedade do objeto que está recebendo os dados do formulário
+            ),
+            const SizedBox(height: 10,),
+            TextFormField(
+              controller: _dateController, // controlador associado
+              readOnly: true,
+              onTap: () async {
+               var data = await showDatePicker(context: context,
+               
+               initialDate: DateTime.now(), 
+               firstDate: DateTime.now(), 
+               lastDate: DateTime.now().add(const Duration(days: 365)),);
+                print(data);
+                if (data!=null) {
+                  String dataFormatada = DateFormat("dd/MM/yyyy").format(data);
+                  _dateController.text = dataFormatada;
                 }
-              }, child: Text("Salvar"))
-            ],
-          ),
-        ));
+              } ,
+
+              keyboardType: TextInputType.datetime,
+              decoration: const InputDecoration(
+                  label: Text("Prazo"), border: OutlineInputBorder()),
+              validator: (value){
+                /**5 - validando o campo do prazo */
+                  try {
+                    DateTime data = DateFormat("dd/MM/yyyy").parse(value!);
+                    if (data.isBefore(DateTime.now()))                  {
+                       return "Data não pode ser no passado!";  
+                    }
+                  } catch(e){
+                    return "Data inválida";
+                  }
+              },                                
+              onSaved: (value) => _tarefa.prazo = DateFormat("dd/MM/yyyy").parse(value!), // 8 - armazenando a data no campo prazo da tarefa 
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              
+              //3- Executa a validação do formulário
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save(); // 4-Solicita ao formulário que salve os dados
+                print("Tarefa digitada: $_tarefa");  // 9- Aqui iremos enviar para o banco de dados
+              }
+
+
+            }, child: const Text("Salvar"))
+          ],
+        ),
+      ),
+    );
   }
 }
