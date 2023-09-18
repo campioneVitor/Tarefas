ebd8d04e6b22208c3d8e9a3e7fcc3cb81b321354
Vitor Campione <vitor.campione@etec.sp.gov.br>
2023-09-18 14:32:08 -0300

    .

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:tarefas/tarefa_page.dart';
 import 'package:tarefas/tarefa_state.dart';
-import 'package:tarefas/tarefas_form.dart';
+import 'package:tarefas/tarefas_helper.dart';
+import 'package:tarefas/tarefas_helper_impl.dart';
 import 'package:tarefas/tarefas_novo.dart';
+
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TarefaState state =TarefaState();
-
+    TarefaState state = TarefaState();
+    TarefasHelper helper = TarefasHelperImpl(); 
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple),
       initialRoute: "/",
       routes: {"/":(context)=>TarefasPage(state: state,),
-               "/add":(context)=>TarefasNovo(state: state,)},
+               "/add":(context)=>TarefasNovo(state: state,helper: helper,)},
+      
     );
   }
 }
--- lib/tarefas_novo.dart
+++ lib/tarefas_novo.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:tarefas/tarefas_form.dart';
 
 import 'tarefa_state.dart';
+import 'tarefas_helper.dart';
 
 class TarefasNovo extends StatelessWidget {
   final TarefaState state;
-  const TarefasNovo({super.key, required this.state});
+  final TarefasHelper helper;
+  const TarefasNovo({super.key, required this.state, required this.helper});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +15,7 @@ class TarefasNovo extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Nova Tarefa"),
       ),
-      body: TarefasForm(state: this.state),
+      body: TarefasForm(state: this.state, helper: helper,),
     );
   }
 }
