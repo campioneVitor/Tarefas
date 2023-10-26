cff6370e397a8a578df4df0795e02027f49c6e70
vitor <vitor.campione@etec.sp.gov.br>
2023-10-26 13:22:28 -0300

    .

--- lib/app.dart
+++ lib/app.dart
@@ -19,7 +19,7 @@ class App extends StatelessWidget {
  
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.orange),
+      theme: ThemeData(primarySwatch: Colors.purple),
       initialRoute: "/",
       routes: {"/":(context)=>TarefasPage(state:state),
                "/add":(context)=>TarefasNovo(state: state,helper: helper,),
